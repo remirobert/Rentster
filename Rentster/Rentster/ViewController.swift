3152a84194d601c6b9534bafc2584900b7200332
wzyddg <haloilove@gmail.com>
2015-10-02 11:32:42 +0800

    init

---
+++ Rentster/Rentster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rentster
+//
+//  Created by Shepard Wang on 15/10/2.
+//  Copyright © 2015年 Shepard Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
