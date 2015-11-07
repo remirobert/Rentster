5227b8b9d754acb0d3cce96e55e245bfd90ebefc
rémi  <remirobert33530@gmail.com>
2015-11-07 15:29:49 +0800

    add center button

--- Rentster/Rentster/RSCustomTabBatViewController.swift
+++ Rentster/Rentster/RSCustomTabBatViewController.swift
@@ -14,16 +14,22 @@ class RSCustomTabBatViewController: UITabBarController {
     lazy var addButton: UIButton! = {
         let addButton = UIButton()
         addButton.backgroundColor = UIColor.redColor()
+        addButton.addTarget(self, action: "createNew", forControlEvents: UIControlEvents.TouchUpInside)
         return addButton
     }()
+    
+    func createNew() {
+        self.performSegueWithIdentifier("createSegue", sender: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
         self.view.addSubview(self.addButton)
         self.addButton.snp_makeConstraints { (make) -> Void in
-//            make
+            make.centerX.equalTo(self.view)
+            make.width.height.equalTo(43)
+            make.bottom.equalTo(0)
         }
     }
 }
